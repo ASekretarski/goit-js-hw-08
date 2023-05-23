a94b71159862760646971d05617472b66af6234b
ASekretarski <artur.sekretarski@gmail.com>
2023-05-23 19:38:56 +0200

    task 01 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,6 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(SimpleLightbox)
 
 const gallery = document.querySelector('.gallery')
 const galleryListItems = [];
